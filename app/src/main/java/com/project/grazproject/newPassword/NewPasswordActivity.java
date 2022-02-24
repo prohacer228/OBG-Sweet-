b39a8eee6f25e3039e016e49cad9323c19c25617
JuliaBak <ov.bakai@gmail.com>
2022-02-24 19:53:45 +0400

    Начальные экраны Не связаны с БД

---
+++ app/src/main/java/com/project/grazproject/newPassword/NewPasswordActivity.java
@@ -0,0 +1,17 @@
+package com.project.grazproject.newPassword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.grazproject.R;
+
+public class NewPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_password);
+
+    }
+}
