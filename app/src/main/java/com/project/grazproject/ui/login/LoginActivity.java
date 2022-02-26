4eccd24d73856ef420a6a8bafb8d2a8f6dd9ebe5
JuliaBak <ov.bakai@gmail.com>
2022-02-26 11:50:46 +0400

    Начальные экраны Не связаны с БД

--- app/src/main/java/com/project/grazproject/ui/login/LoginActivity.java
+++ app/src/main/java/com/project/grazproject/ui/login/LoginActivity.java
@@ -1,18 +1,8 @@
 package com.project.grazproject.ui.login;
 
 import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.content.Intent;
-import android.media.audiofx.PresetReverb;
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
@@ -24,28 +14,28 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.project.grazproject.R;
-import com.project.grazproject.about.AboutActivity;
+import com.project.grazproject.databinding.ActivityLoginBinding;
 import com.project.grazproject.register.RegisterActivity;
 import com.project.grazproject.resetPassword.ResetPasswordActivity;
-import com.project.grazproject.ui.login.LoginViewModel;
-import com.project.grazproject.ui.login.LoginViewModelFactory;
-import com.project.grazproject.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
     private ActivityLoginBinding binding;
 
-    //TODO : добавить возможность скрывать и показывать пароль, "закрывающий глаз"
-    //TODO : добавить недостающие экраны
-    //TODO : добавить дизайн
-    //TODO : связать с БД
-    //TODO : добавить возможность взаимодействия с картами
-    //TODO : выяснить и добавить правила проекта "Сознательный гражданин"
-    //TODO : установить ограничения ввода на поля (например, пароль). Так, в некоторых полях можно использовать
-    // только кириллицу и т.п.
-    //TODO : сделать выпадающий список улиц по первой букве
+    EditText usernameEditText;
+    EditText passwordEditText;
+    Button loginButton;
+    Button registerButton;
+    ProgressBar loadingProgressBar;
+    TextView forgotPassword;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -57,12 +47,14 @@ public class LoginActivity extends AppCompatActivity {
         loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
 
-        final EditText usernameEditText = binding.username;
-        final EditText passwordEditText = binding.password;
-        final Button loginButton = binding.login;
-        final Button registerButtonLog = binding.registerButtonLog;
-        final ProgressBar loadingProgressBar = binding.loading;
-        final TextView forgotPassword = findViewById(R.id.forgotPassword);
+       usernameEditText = binding.username;
+       passwordEditText = binding.password;
+       loginButton = binding.login;
+       registerButton = binding.registerButtonLog;
+       loadingProgressBar = binding.loading;
+       forgotPassword = binding.forgotPassword;
+
+      //  forgotPassword = findViewById(R.id.forgotPassword);
 
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
             @Override
@@ -142,7 +134,7 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         //Переход на страницу регистрации
-        registerButtonLog.setOnClickListener(new View.OnClickListener() {
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
@@ -150,11 +142,15 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        // TODO : сделать переход на экран восстановления пароля. Пока вылетает
+        //go to Reset Password activity
+
        forgotPassword.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
                Intent intent = new Intent(LoginActivity.this, ResetPasswordActivity.class);
                startActivity(intent);
+
            }
        });
     }
